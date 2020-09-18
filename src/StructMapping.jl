6a354798c1a34e21dfa925b91ac6296ca7895d8c
matsueushi <matsueushi@gmail.com>
2020-09-18 23:29:17 +0900

    prototype

--- src/StructMapping.jl
+++ src/StructMapping.jl
@@ -1,7 +1,64 @@
 module StructMapping
+using Parameters
 
-include("mapping.jl")
+export keytosymbol, structmap
 
-export keytosymbol
+_keytosymbol(x) = x
+_keytosymbol(v::AbstractVector) = keytosymbol.(v)
+_keytosymbol(d::AbstractDict) = Dict(Symbol(k) => _keytosymbol(v) for (k, v) in pairs(d))
+
+"""
+    keytosymbol(x)
+
+Convert type of keys of the given dictionary to `Symbol` recursively.
+
+# Examples
+```jldoctest
+julia> keytosymbol(Dict("a"=>1, "b"=>1))
+Dict{Symbol,Int64} with 2 entries:
+  :a => 1
+  :b => 1
+
+julia> keytosymbol(Dict("a"=>1, "b"=>Dict("c"=>2, "d"=>3)))
+Dict{Symbol,Any} with 2 entries:
+  :a => 1
+  :b => Dict(:d=>3,:c=>2)
+
+julia> keytosymbol(Dict("a"=>1, "b"=>[Dict("c"=>2), Dict("d"=>3)]))
+Dict{Symbol,Any} with 2 entries:
+  :a => 1
+  :b => [Dict(:c=>2), Dict(:d=>3)]
+```
+"""
+keytosymbol(d::AbstractDict) = _keytosymbol(d)
+
+
+_squeeze_type(::Type{T}) where T = T
+_squeeze_type(::Type{Vector{T}}) where T = T
+_squeeze_type(::Type{Union{T, Nothing}}) where T = T
+
+
+
+function _findmap(T::Type)
+    mapdict = Dict()
+    for name in fieldnames(T)
+        sym = _squeeze_type(fieldtype(T, name))
+        if !isdefined(Base, Symbol(sym))
+            mapdict[name] = sym
+        end
+    end
+    return mapdict
+end
+
+_structmap(T::Type, d::AbstractDict) = T(;d...)
+_structmap(T::Type, v::AbstractVector) = _structmap.(T, v)
+function _structmap(T::Type, d::AbstractDict, m::AbstractDict)
+    for (k, v) in pairs(m)
+        d[k] = _structmap(v, d[k])
+    end
+    return T(;d...)
+end
+
+structmap(T::Type, d::AbstractDict) = _structmap(T, keytosymbol(d))
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,26 @@
-using Test, Documenter, StructMapping
-DocMeta.setdocmeta!(StructMapping, :DocTestSetup, :(using StructMapping); recursive=true)
-doctest(StructMapping)
+using Test, Documenter, Parameters, StructMapping
+
+@testset "doctest" begin
+    DocMeta.setdocmeta!(StructMapping, :DocTestSetup, :(using StructMapping); recursive=true)
+    doctest(StructMapping)
+end
+
+@with_kw struct A
+    a::Float64
+    b::String
+end
+
+@with_kw struct B
+    a::A
+    b::Int64
+end
+
+@testset "simple" begin
+    dict_a = Dict("b"=>"b", "a"=>1.0)
+    dict_b = Dict("a"=>dict_a, "b"=>4)
+
+    StructMapping._structmap(::Type{B}, d::AbstractDict) = StructMapping._structmap(B, d, StructMapping._findmap(B))
+
+    @test structmap(A, dict_a) == A(1.0, "b")
+    @test structmap(B, dict_b) == B(A(1.0, "b"), 4)
+end
